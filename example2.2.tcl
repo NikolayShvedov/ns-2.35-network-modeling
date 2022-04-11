0ff0ef5eeb35ef2dfcee01cca2252c4dbe7c9713
Nikolay Shvedov <shved.nikolay.18@mail.ru>
2022-04-11 20:28:01 +0300

    fix
    fix

--- example2.2.tcl
+++ example2.2.tcl
@@ -1,110 +1,241 @@
-#Create a simulator object
-set ns [new Simulator]
+set simulation_time 60
 
-#Define different colors for data flows (for NAM) 
-$ns color 1 Blue
-$ns color 2 Red
+  set ns [new Simulator]
+  set nf [open out.nam w]
+  $ns namtrace-all $nf
 
-#Open the NAM trace file
-set nf [open out.nam w]
-$ns namtrace-all $nf
+  for {set i_ 1} {$i_ < 3} {incr i_} {
+   set current_tr($i_) [open queuelen-$i_.tr w]
+   set average_tr($i_) [open average-$i_.tr w]
+  }
 
-#Define a 'finish' procedure
-proc finish {} {
-        global ns nf
-        $ns flush-trace
-        #Close the NAM trace file
-        close $nf
-        #Execute NAM on the trace file
-        exec nam out.nam &
-        exit 0
+  proc finish {} {
+      global ns nf current_tr average_tr
+      $ns flush-trace
+      close $nf
+      exec nam -a out.nam &
+      for {set i_ 1} {$i_ < 3} {incr i_} {
+       close $current_tr($i_)
+       close $average_tr($i_)
+       exec xgraph queuelen-$i_.tr average-$i_.tr -geometry 600x450 &
+      }
+      exit 0
+  }
+
+  set n0     [$ns node]
+  set n1     [$ns node]
+  set n2     [$ns node]
+  set n3     [$ns node]
+  set n4     [$ns node]
+  set n5     [$ns node]
+  set n6     [$ns node]
+
+  $ns color 1  Green
+  $ns color 2  Red
+
+#   n0            n5
+#     \          / 
+#      n2--n3--n4
+#     /          \
+#   n1            n6
+#
+     
+  $ns duplex-link  $n0 $n2 2Mb 15ms DropTail
+  $ns duplex-link  $n1 $n2 2Mb 15ms DropTail
+
+  $ns simplex-link $n2 $n3 2Mb 15ms dsRED/edge
+  $ns simplex-link $n3 $n2 2Mb 15ms dsRED/core 
+  $ns simplex-link $n3 $n4 409600 10ms dsRED/core 
+  $ns simplex-link $n4 $n3 409600 10ms dsRED/edge
+
+  $ns duplex-link  $n4 $n5 2Mb 60ms DropTail
+  $ns duplex-link  $n4 $n6 409600 60ms DropTail
+
+  $ns duplex-link-op $n0     $n2 orient right-down
+  $ns duplex-link-op $n1     $n2 orient right-up
+  $ns duplex-link-op $n2     $n3 orient right
+  $ns duplex-link-op $n3     $n4 orient right
+  $ns duplex-link-op $n4     $n5 orient right-up
+  $ns duplex-link-op $n4     $n6 orient right-down
+
+  $ns queue-limit $n3 $n4 100
+  $ns queue-limit $n4 $n5 100
+
+  $ns duplex-link-op $n3 $n4 queuePos 0.5
+
+  set qE1C [[$ns link $n2 $n3] queue]  
+  set qE2C [[$ns link $n4 $n3] queue]  
+  set qCE1 [[$ns link $n3 $n2] queue]  
+  set qCE2 [[$ns link $n3 $n4] queue]  
+
+  $qE1C meanPktSize 1000
+  $qE1C set numQueues_ 3
+  $qE1C setNumPrec 3    
+  $qE1C setSchedularMode RR 
+  $qE1C setMREDMode RIO-C
+  
+  $qE1C addPolicyEntry [$n0 id] -1 TokenBucket 18  5000000  4000  
+  $qE1C addPolicyEntry [$n1 id] -1 TokenBucket 26  5000000  4000
+
+  $qE1C addPolicerEntry TokenBucket 18 20  
+  $qE1C addPolicerEntry TokenBucket 26 28  
+
+  $qE1C addPHBEntry 18       1           0
+  $qE1C addPHBEntry 20       1           1
+
+  $qE1C addPHBEntry 26       2           0
+  $qE1C addPHBEntry 28       2           1
+
+  $qE1C configQ 0           0            8             16             0.02
+  $qE1C configQ 0           1            4              8             0.10
+  $qE1C configQ 0           2            2              4             0.50
+
+  $qE1C configQ 1           0            8             16             0.02
+  $qE1C configQ 1           1            4              8             0.10
+  $qE1C configQ 1           2            2              4             0.50
+
+  $qE1C configQ 2           0            8             16             0.02
+  $qE1C configQ 2           1            4              8             0.10
+  $qE1C configQ 2           2            2              4             0.50
+
+
+  $qE2C meanPktSize 1000
+  $qE2C set numQueues_ 3
+  $qE2C setNumPrec 3    
+  $qE2C setSchedularMode RR 
+  $qE2C setMREDMode RIO-C
+  
+  $qE2C addPolicyEntry -1 [$n0 id] TokenBucket 18  5000000  4000  
+  $qE2C addPolicyEntry -1 [$n1 id] TokenBucket 26  5000000  4000
+
+  $qE2C addPolicerEntry TokenBucket 18 20  
+  $qE2C addPolicerEntry TokenBucket 26 28  
+
+  $qE2C addPHBEntry 18       1           0
+  $qE2C addPHBEntry 20       1           1
+
+  $qE2C addPHBEntry 26       2           0
+  $qE2C addPHBEntry 28       2           1
+
+  $qE2C configQ 0           0            8             16             0.02
+  $qE2C configQ 0           1            4              8             0.10
+  $qE2C configQ 0           2            2              4             0.50
+
+  $qE2C configQ 1           0            8             16             0.02
+  $qE2C configQ 1           1            4              8             0.10
+  $qE2C configQ 1           2            2              4             0.50
+
+  $qE2C configQ 2           0            8             16             0.02
+  $qE2C configQ 2           1            4              8             0.10
+  $qE2C configQ 2           2            2              4             0.50
+
+  $qCE1 meanPktSize 1000
+  $qCE1 set numQueues_ 3 
+  $qCE1 setNumPrec 3
+  $qCE1 setSchedularMode RR 
+  $qCE1 setMREDMode RIO-C
+
+  $qCE1 addPHBEntry 18       1           0
+  $qCE1 addPHBEntry 20       1           1
+  $qCE1 addPHBEntry 22       1           2
+
+  $qCE1 addPHBEntry 26       2           0
+  $qCE1 addPHBEntry 28       2           1
+  $qCE1 addPHBEntry 30       2           2
+
+  $qCE1 configQ 0           0            8             16             0.02
+  $qCE1 configQ 0           1            4              8             0.10
+  $qCE1 configQ 0           2            2              4             0.50
+
+  $qCE1 configQ 1           0            8             16             0.02
+  $qCE1 configQ 1           1            4              8             0.10
+  $qCE1 configQ 1           2            2              4             0.50
+  
+  $qCE1 configQ 2           0            8             16             0.02
+  $qCE1 configQ 2           1            4              8             0.10
+  $qCE1 configQ 2           2            2              4             0.50
+
+  $qCE2 meanPktSize 1000
+  $qCE2 set numQueues_ 3
+  $qCE2 setNumPrec 3 
+  $qCE2 setSchedularMode RR  
+  $qCE2 setMREDMode RIO-D
+
+  $qCE2 addPHBEntry 18       1           0
+  $qCE2 addPHBEntry 20       1           1
+  $qCE2 addPHBEntry 22       1           2
+
+  $qCE2 addPHBEntry 26       2           0
+  $qCE2 addPHBEntry 28       2           1
+  $qCE2 addPHBEntry 30       2           2
+
+  $qCE2 configQ 0           0            8             16             0.01
+  $qCE2 configQ 0           1            4              8             0.02
+  $qCE2 configQ 0           2            2              4             0.05
+
+  $qCE2 configQ 1           0            16            32             0.01
+  $qCE2 configQ 1           1            8             16             0.02
+  $qCE2 configQ 1           2            4              8             0.04
+
+  $qCE2 configQ 2           0            4             12             0.01
+  $qCE2 configQ 2           1            4              8             0.02
+  $qCE2 configQ 2           2            2              4             0.04
+ 
+  set udp0 [new Agent/UDP]
+  $udp0 set class_ 1 
+  set cbr0 [new Application/Traffic/CBR]
+  $ns attach-agent $n0 $udp0
+  $cbr0 attach-agent $udp0
+  $cbr0 set packetSize_ 1000
+  $cbr0 set interval_ 0.005
+  set null0 [new Agent/LossMonitor]
+  $ns attach-agent $n5 $null0
+  $ns connect $udp0 $null0
+
+  set udp1 [new Agent/UDP]
+  $udp1 set class_ 2 
+  set cbr1 [new Application/Traffic/CBR]
+  $ns attach-agent $n1 $udp1
+  $cbr1 attach-agent $udp1
+  $cbr1 set packetSize_ 1000
+  $cbr1 set interval_ 0.005
+  set null1 [new Agent/LossMonitor]
+  $ns attach-agent $n6 $null1
+  $ns connect $udp1 $null1
+
+proc ds_stats {} {
+    global qE1C qCE1 qE2C qCE2
+    puts "\n\n**** edge1 -> core ****"
+    $qE1C printStats
+    puts "\n\n**** core -> edge2 ****"
+    $qCE2 printStats
+    puts "\n\n**** edge2 -> core ****"
+    $qE2C printStats
+    puts "\n\n**** core -> edge1 ****"
+    $qCE1 printStats
 }
 
-#Create four nodes
-set n1 [$ns node]
-set n2 [$ns node]
-set n3 [$ns node]
-set n4 [$ns node]
-set n5 [$ns node]
-set n6 [$ns node]
+proc queue_stats {queue} {
+    global current_tr average_tr
+    set time 0.1
+    set ns [Simulator instance]
+    set now [$ns now]
+    for {set i_ 1} {$i_ < 3} {incr i_} {
+     set current_ [$queue getCurrent $i_]
+     set average_ [$queue getAverage $i_]
+     puts $current_tr($i_)  "$now $current_"
+     puts $average_tr($i_)  "$now $average_"
+    }
+    $ns at [expr $now+$time] "queue_stats $queue"   
+}
 
-#Create links between the nodes
-$ns duplex-link $n1 $n2 1.5Mb 10ms DropTail
-$ns duplex-link $n1 $n4 1.5Mb 10ms DropTail
-$ns duplex-link $n2 $n3 1.5Mb 10ms SFQ
-$ns duplex-link $n4 $n5 1.5Mb 10ms SFQ
-$ns duplex-link $n5 $n6 1.5Mb 10ms DropTail
-$ns duplex-link $n3 $n4 1.5Mb 60ms DropTail
-$ns duplex-link $n1 $n6 1.5Mb 60ms SFQ
+  $ns at 0.0 "$cbr0 start"
+  $ns at 0.0 "$cbr1 start"
+  $ns at 0.0 "queue_stats $qCE2"
+  $ns at $simulation_time  "$cbr0 stop"
+  $ns at $simulation_time  "$cbr1 stop"
+  $ns at [expr $simulation_time + 5] "ds_stats"
+  $ns at [expr $simulation_time + 5] "finish"
 
-#Give node position (for NAM)
-$ns duplex-link-op $n1 $n2 orient right
-$ns duplex-link-op $n1 $n4 orient down
-$ns duplex-link-op $n2 $n3 orient right
-$ns duplex-link-op $n4 $n5 orient right
-$ns duplex-link-op $n5 $n6 orient right
-$ns duplex-link-op $n3 $n4 orient left-down
-$ns duplex-link-op $n1 $n6 orient right-down
-
-#Monitor the queue for link (n2-n3, n4-n5, n1-n6). (for NAM)
-$ns duplex-link-op $n3 $n4 queuePos 0.5
-$ns duplex-link-op $n4 $n5 queuePos 0.5
-$ns duplex-link-op $n2 $n3 queuePos 0.5
-
-#Create a UDP agent and attach it to node n1
-set udp0 [new Agent/UDP]
-$udp0 set class_ 1
-$ns attach-agent $n1 $udp0
-
-#Create a CBR traffic source and attach it to udp0
-set cbr0 [new Application/Traffic/CBR]
-$cbr0 set packetSize_ 328
-$cbr0 set interval_ 0.005
-$cbr0 attach-agent $udp0
-
-#Create a UDP agent and attach it to node n5
-set udp1 [new Agent/UDP]
-$udp1 set class_ 2
-$ns attach-agent $n5 $udp1
-
-#Create a CBR traffic source and attach it to udp1
-set cbr1 [new Application/Traffic/CBR]
-$cbr1 set packetSize_ 328
-$cbr1 set interval_ 0.005
-$cbr1 attach-agent $udp1
-
-#Create a TCP agent and attaching it to node n4
-set tcp1 [new Agent/TCP]
-$ns attach-agent $n4 $tcp1
-
-#Create an FTP application and attaching it to the tcp1 agent
-set ftp [new Application/FTP]
-$ftp attach-agent $tcp1
-
-#Create a Null agent (a traffic sink) and attach it to node n3
-set null0 [new Agent/Null]
-$ns attach-agent $n3 $null0
-
-#Create a recipient agent for tcp1
-set sink1 [new Agent/TCPSink]
-$ns attach-agent $n3 $sink1
-
-#Connect the traffic sources with the traffic sink
-$ns connect $udp0 $null0
-$ns connect $udp1 $null0  
-$ns connect $tcp1 $sink1
-
-#Schedule events for the CBR agents
-$ns at 0.5 "$cbr0 start"
-$ns at 1.0 "$cbr1 start"
-$ns at 1.0 "$ftp start"
-$ns at 4.0 "$ftp stop"
-$ns at 4.0 "$cbr1 stop"
-$ns at 4.5 "$cbr0 stop"
-
-#Call the finish procedure after 17 seconds of simulation time
-$ns at 17.0 "finish"
-
-#Run the simulation
-$ns run
+  $ns run
 
